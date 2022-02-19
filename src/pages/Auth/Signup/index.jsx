f50eba752e1ce9f44c9bec7556cf07d5a0f39177
Richell <rodrigorichell@gmail.com>
2022-02-19 18:46:24 -0300

    novo

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -1,13 +1,17 @@
 import './style.css';
 
 export function Signup() {
-	return <div className="signup-div">
-		<section className="signup-titulo">Registrar Conta</section>
-		<section className="signup-subtitulo">Registro de e-mail</section>
-		<section className="signup-inputBox">Insira o e-mail aqui</section>
-		<section className="signup-subtitulo">Senha</section>
-		<section className="signup-imputBox">Insira a senha aqui</section>
-		<section className="signup-boxSelect">Eu concordo com os termos e condições</section>
-		<section className="signup-button">Registrar conta</section>
-	</div>;
+	return (
+		<div className="signup-div">
+			<section className="signup-titulo">Registrar Conta</section>
+			<section className="signup-subtitulo">Registro de e-mail</section>
+			<input placeholder="Insida seu e-mail" className="signup-inputBox" />
+			<section className="signup-subtitulo">Senha</section>
+			<input placeholder="Escolha uma senha" className="signup-inputBox" />
+			<br></br>
+			<input type="checkbox" className="signup-boxSelect" />Eu concordo com os termos e condições
+			<br></br>
+			<button className="signup-button">Registrar conta</button>
+		</div>
+	);
 }
