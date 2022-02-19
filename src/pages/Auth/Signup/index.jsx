dbaa751c15697186b323dc40785c8b9a3a728498
Richell <rodrigorichell@gmail.com>
2022-02-19 19:18:41 -0300

    update

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -4,13 +4,15 @@ export function Signup() {
 	return (
 		<div className="signup-div">
 			<section className="signup-titulo">Registrar Conta</section>
+			<br>
+			</br>
 			<section className="signup-subtitulo">Registro de e-mail</section>
 			<div className="signup-div-input">
-				<input className= "signup-input" type="teste" placeholder="insira o e-mail aqui!" />
+				<input className= "signup-input" type="teste" placeholder="Insira o e-mail aqui." />
 			</div>
 			<section className="signup-subtitulo">Senha</section>
 			<div className="signup-div-input">
-				<input className= "signup-input" type="teste" placeholder="insira o e-mail aqui!" />
+				<input className= "signup-input" type="teste" placeholder="Escolha uma senha." />
 				<div className="signup-btn">
 					show
 				</div>
