108e3b399f119dca85c5b15f345b578480afe21b
Eduardo Rerick <eduardorerick@gmail.com>
2022-02-19 20:08:48 -0300

    removed unused import

--- src/context/Auth/index.jsx
+++ src/context/Auth/index.jsx
@@ -1,14 +1,11 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
 	const userData = JSON.parse(localStorage.getItem('user')) || {};
 	const [ user, setUser ] = useState(userData);
-	
-	useEffect(() => {
-		setUser(userData);
-	}, []);
+
 
 	return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
 };
