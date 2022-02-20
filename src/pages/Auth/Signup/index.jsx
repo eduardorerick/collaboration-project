f9ff37c5d234d4a400dadef544f27305501d2ebb
Richell <rodrigorichell@gmail.com>
2022-02-20 18:00:43 -0300

    VOX MACHINA

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -2,14 +2,19 @@ import './style.css';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import GoogleIcon from '@mui/icons-material/Google';
 import { useState } from 'react';
-
+import { useCreateUser } from '../../../services/firebase/firebase';
 
 export function Signup() {
-	const [email, setEmail] = useState('')
-	const [pass, setPass] = useState('')
-	function cadastrar(email, senha){
-		let dados= [email, senha];
-		return console.log(dados);
+	const [email, setEmail] = useState('');
+	const [pass, setPass] = useState('');
+	const cadastro = useCreateUser();
+		
+	function cadastrar(){
+		try{
+		cadastro(email, pass);
+	}	catch (err){
+			console.log(err);
+		}
 	}
 
 	return (
@@ -32,7 +37,7 @@ export function Signup() {
 			<br></br>
 			<input type="checkbox" className="signup-boxSelect" />Eu concordo com os termos e condições
 			<br></br><br></br>
-			<button onClick={cadastrar(email, pass)} className="signup-button">Registrar conta</button>
+			<button onClick={cadastrar} className="signup-button">Registrar conta</button>
 			<button className='signup-button'>Registrar com Google<GoogleIcon/></button>
 		</div>
 	);
