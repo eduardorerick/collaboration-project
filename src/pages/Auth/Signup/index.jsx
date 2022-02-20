9dff5775f4f338c072fd44cdafae16d67b2eb10f
Richell <rodrigorichell@gmail.com>
2022-02-20 18:17:42 -0300

    Fix/cadastro (#10)
    * KAMEAMEHA
    
    * MÃO DO SCALAN
    
    * VOX MACHINA

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -1,6 +1,22 @@
 import './style.css';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import GoogleIcon from '@mui/icons-material/Google';
+import { useState } from 'react';
+import { useCreateUser } from '../../../services/firebase/firebase';
 
 export function Signup() {
+	const [email, setEmail] = useState('');
+	const [pass, setPass] = useState('');
+	const cadastro = useCreateUser();
+		
+	function cadastrar(){
+		try{
+		cadastro(email, pass);
+	}	catch (err){
+			console.log(err);
+		}
+	}
+
 	return (
 		<div className="signup-div">
 			<section className="signup-titulo">Registrar Conta</section>
@@ -8,20 +24,21 @@ export function Signup() {
 			<br></br>
 			<section className="signup-subtitulo">Registro de e-mail</section>
 			<div className="signup-div-input">
-				<input className= "signup-input" type="teste" placeholder="Insira o e-mail aqui." />
+				<input value={email} onChange= {(evt)=> setEmail(evt.target.value)} className= "signup-input" type="email" placeholder="Insira o e-mail aqui." />
 			</div>
 			<br></br>
 			<section className="signup-subtitulo">Insira uma senha</section>
 			<div className="signup-div-input">
-				<input className= "signup-input" type="teste" placeholder="Escolha uma senha." />
-				<div className="signup-btn">
-					show
-				</div>
+				<input value={pass} onChange= {(evt)=> setPass(evt.target.value)} className= "signup-input" type="password" placeholder="Escolha uma senha." />
+				<button id="signup-olhar" className="signup-btn">
+					<VisibilityOffIcon/>
+				</button>
 			</div>
 			<br></br>
 			<input type="checkbox" className="signup-boxSelect" />Eu concordo com os termos e condições
 			<br></br><br></br>
-			<button className="signup-button">Registrar conta</button>
-			</div>
+			<button onClick={cadastrar} className="signup-button">Registrar conta</button>
+			<button className='signup-button'>Registrar com Google<GoogleIcon/></button>
+		</div>
 	);
 }
