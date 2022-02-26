67af1da0844e1e63efc8d86f51c0c681b2de991b
Eduardo Rerick <eduardorerick@gmail.com>
2022-02-26 15:43:12 -0300

    added toggle password

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -1,18 +1,19 @@
 import './style.css';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { VisibilityOff, Visibility } from '@mui/icons-material';
 import GoogleIcon from '@mui/icons-material/Google';
 import { useState } from 'react';
 import { useCreateUser } from '../../../services/firebase/firebase';
 
 export function Signup() {
-	const [email, setEmail] = useState('');
-	const [pass, setPass] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ pass, setPass ] = useState('');
+	const [ isPasswordVisible, setIsPasswordVisible ] = useState(false);
 	const cadastro = useCreateUser();
-		
-	function cadastrar(){
-		try{
-		cadastro(email, pass);
-	}	catch (err){
+
+	function cadastrar() {
+		try {
+			cadastro(email, pass);
+		} catch (err) {
 			console.log(err);
 		}
 	}
@@ -21,24 +22,42 @@ export function Signup() {
 		<div className="signup-div">
 			<section className="signup-titulo">Registrar Conta</section>
 			<section className="signup-sublinhar"> </section>
-			<br></br>
+			<br />
 			<section className="signup-subtitulo">Registro de e-mail</section>
 			<div className="signup-div-input">
-				<input value={email} onChange= {(evt)=> setEmail(evt.target.value)} className= "signup-input" type="email" placeholder="Insira o e-mail aqui." />
+				<input
+					value={email}
+					onChange={(evt) => setEmail(evt.target.value)}
+					className="signup-input"
+					type="email"
+					placeholder="Insira o e-mail aqui."
+				/>
 			</div>
-			<br></br>
+			<br />
 			<section className="signup-subtitulo">Insira uma senha</section>
 			<div className="signup-div-input">
-				<input value={pass} onChange= {(evt)=> setPass(evt.target.value)} className= "signup-input" type="password" placeholder="Escolha uma senha." />
-				<button id="signup-olhar" className="signup-btn">
-					<VisibilityOffIcon/>
+				<input
+					value={pass}
+					onChange={(evt) => setPass(evt.target.value)}
+					className="signup-input"
+					type={isPasswordVisible ? 'text' : 'password'}
+					placeholder="Escolha uma senha."
+				/>
+				<button id="signup-olhar" className="signup-btn-password" onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+					{isPasswordVisible && <VisibilityOff />}
+					{!isPasswordVisible && <Visibility />}
 				</button>
 			</div>
-			<br></br>
+			<br />
 			<input type="checkbox" className="signup-boxSelect" />Eu concordo com os termos e condições
-			<br></br><br></br>
-			<button onClick={cadastrar} className="signup-button">Registrar conta</button>
-			<button className='signup-button'>Registrar com Google<GoogleIcon/></button>
+			<br />
+			<br />
+			<button onClick={cadastrar} className="signup-button">
+				Registrar conta
+			</button>
+			<button className="signup-button">
+				Registrar com Google<GoogleIcon />
+			</button>
 		</div>
 	);
 }
