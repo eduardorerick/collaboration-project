f87383e16a273fde54ed46d6b71c4d41baf034cd
Eduardo Rerick <eduardorerick@gmail.com>
2022-03-05 18:34:03 -0300

    added tasklist

--- src/pages/Portal/TaskList/index.jsx
+++ src/pages/Portal/TaskList/index.jsx
@@ -1,24 +1,75 @@
-import { TaskBox } from '../../../components/TaskBox';
-import './style.css';
+import { CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { TaskBox } from "../../../components/TaskBox";
+import {
+  useUpdateUserCollection,
+  useCreateNewTask,
+  useUserCollection,
+} from "../../../services/firebase/firebase";
+import "./style.css";
 
 export function TaskList() {
-	return (
-		<section className="tasklist-conteudo">
-			<div>
-				<h1>Adicionar novas tarefas</h1>
-				<div className="tasklist-novatarefa">
-					<input type="text" id="Nova tarefa" placeholder="Adicionar tarefa..." />
-					<button>+</button>
-				</div>
-				<div>
-					<TaskBox title="Teste 2" />
-				</div>
-			</div>
+  const [taskTitle, setTaskTitle] = useState("");
+  const [value, loading, error] = useUserCollection();
+  const updateTask = useUpdateUserCollection();
 
-			<div className="tasklist-tarefaconcluida">
-				<h1>Tarefas completas</h1>
-				<TaskBox title="Teste" isCompleted />
-			</div>
-		</section>
-	);
+  const createNewTask = useCreateNewTask();
+
+  async function handleCompleteTask(title) {
+    try {
+      await updateTask(title);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleNewTask() {
+    try {
+      createNewTask(taskTitle);
+      setTaskTitle("");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <section className="tasklist-conteudo">
+      <div>
+        <h1>Adicionar novas tarefas</h1>
+        <div className="tasklist-novatarefa">
+          <input
+            type="text"
+            id="Nova tarefa"
+            placeholder="Adicionar tarefa..."
+            value={taskTitle}
+            onChange={(evt) => setTaskTitle(evt.target.value)}
+          />
+          <button onClick={handleNewTask}>+</button>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          {loading && <CircularProgress sx={{ width: "100%", mt: 3 }} />}
+          {value &&
+            value
+              .filter(({ isCompleted }) => !isCompleted)
+              .map((task, idx) => (
+                <TaskBox
+                  key={idx}
+                  title={task.title}
+                  handleComplete={() => handleCompleteTask(task.title)}
+                />
+              ))}
+        </div>
+      </div>
+
+      <div className="tasklist-tarefaconcluida">
+        <h1>Tarefas completas</h1>
+        {value &&
+          value
+            .filter(({ isCompleted }) => isCompleted)
+            .map((task, idx) => (
+              <TaskBox key={idx} title={task.title} isCompleted />
+            ))}
+      </div>
+    </section>
+  );
 }
--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,17 +1,33 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { getFirestore, doc, setDoc, addDoc, collection, onSnapshot, query } from 'firebase/firestore';
-import { useSnackbar } from 'notistack';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/Auth';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+import { useSnackbar } from "notistack";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/Auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
@@ -20,75 +36,106 @@ export const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 
 export function useCreateUser() {
-	const { enqueueSnackbar } = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
 
-	return (email, password) =>
-		createUserWithEmailAndPassword(auth, email, password)
-			.then(async (userCredential) => {
-				const user = userCredential.user;
-				await setDoc(doc(db, 'users', user.uid), {
-					email: user.email,
-					id: user.uid
-				});
-				enqueueSnackbar('Usuário criado com sucesso!', { variant: 'success' });
-			})
-			.catch((error) => {
-				enqueueSnackbar(error.message, { variant: 'error' });
-				// ..
-			});
+  return (email, password) =>
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        const user = userCredential.user;
+        await setDoc(doc(db, "users", user.uid), {
+          email: user.email,
+          id: user.uid,
+        });
+        enqueueSnackbar("Usuário criado com sucesso!", { variant: "success" });
+      })
+      .catch((error) => {
+        enqueueSnackbar(error.message, { variant: "error" });
+        // ..
+      });
 }
 
 export function useLogin() {
-	const { setUser } = useAuth();
-	const { enqueueSnackbar } = useSnackbar();
-	const navigate = useNavigate();
+  const { setUser } = useAuth();
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
 
-	return (email, password, path) =>
-		signInWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-				const user = userCredential.user;
-				setUser(user);
-				localStorage.setItem('user', JSON.stringify(user));
-				navigate(path);
-				// ...
-			})
-			.catch((error) => {
-				enqueueSnackbar(error.message, { variant: 'error' });
-			});
+  return (email, password, path) =>
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user);
+        localStorage.setItem("user", JSON.stringify(user));
+        navigate(path);
+        // ...
+      })
+      .catch((error) => {
+        enqueueSnackbar(error.message, { variant: "error" });
+      });
 }
 
 export function useCreateNewTask() {
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	return async (title) => {
-		await addDoc(collection(db, `users/${user.uid}/tasks`), {
-			title: title,
-			isCompleted: false
-		});
-	};
+  return async (title) => {
+    await addDoc(collection(db, `users/${user.uid}/tasks`), {
+      title: title,
+      isCompleted: false,
+    });
+  };
 }
 
 export function useTasks() {
-	const { user } = useAuth();
-	const docs = [];
+  const { user } = useAuth();
+  const docs = [];
 
-	const q = query(collection(db, `users/${user.uid}/tasks`));
-	onSnapshot(q, (snapshot) => {
-		snapshot.forEach((docs) => {
-			docs.push(docs.data());
-		});
-	});
+  const q = query(collection(db, `users/${user.uid}/tasks`));
+  onSnapshot(q, (snapshot) => {
+    snapshot.forEach((docs) => {
+      docs.push(docs.data());
+    });
+  });
 
-	return docs;
+  return docs;
 }
 
 export function useSignOut() {
-	const { setUser } = useAuth();
-	const navigate = useNavigate();
+  const { setUser } = useAuth();
+  const navigate = useNavigate();
 
-	return () => {
-		setUser({});
-		localStorage.clear();
-		navigate('/auth/login');
-	};
+  return () => {
+    setUser({});
+    localStorage.clear();
+    navigate("/auth/login");
+  };
+}
+
+export function useUserCollection() {
+  const { user } = useAuth();
+
+  return useCollectionData(collection(db, `users/${user.uid}/tasks`), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+}
+
+export function useUpdateUserCollection() {
+  const { user } = useAuth();
+
+  return async (title) => {
+    const queryDoc = query(
+      collection(db, `users/${user.uid}/tasks`),
+      where("title", "==", title)
+    );
+    const selectedDoc = await getDocs(queryDoc);
+
+    let docId = "";
+    selectedDoc.forEach((doc) => {
+      docId = doc.id;
+    });
+
+    let docRef = doc(db, `users/${user.uid}/tasks/${docId}`);
+
+    await updateDoc(docRef, {
+      isCompleted: true,
+    });
+  };
 }
