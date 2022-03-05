dce59f0e72a318561002e0c9cd1623edfb6686ff
Richell <rodrigorichell@gmail.com>
2022-03-05 18:36:13 -0300

    profile page

--- src/pages/Portal/Profile/index.jsx
+++ src/pages/Portal/Profile/index.jsx
@@ -1,8 +1,11 @@
-import './style.css';
+import './style.scss';
 export function Profile() {
 	return (
-		<div>
-			<h1>Todo UserSearch ~</h1>
+		<div className='div-profile-cabecalho'>
+			<h1>Collaboration Project</h1>
+			<h2>Tarefas</h2>
+			<h2>Profile</h2>
+			<h2>LogOut</h2>
 		</div>
 	);
 }
